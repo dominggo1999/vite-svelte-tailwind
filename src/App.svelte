4bf18ddf2346734f146ada9f7ad221743ec0706d
dominggo1999 <arnolddominggo19@gmail.com>
2022-05-04 15:02:08 +0700

    init

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import TailwindCSS from "./TailwindCSS.svelte";
+</script>
+
+<TailwindCSS/>
+
+<div class="w-full h-screen flex flex-col gap-y-2 justify-center items-center">
+  <img src="/svelte.png" alt="Svelte Logo" width="100">
+  <p class="text-[#ff3e00] text-2xl">Vite Svelte Tailwind Template</p>
+  <p class="text-base">Created By <a class="text-[#ff3e00] font-semibold" target="_blank" rel="noopener noreferrer" href="https://github.com/dominggo1999">dominggo1999</a></p>
+</div>
+
+
+
